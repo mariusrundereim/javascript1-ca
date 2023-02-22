663ced143496485017c2cf588f294dfe9309d2bd
mariusrundereim <marius.rundereim@gmail.com>
2023-02-22 10:32:25 +0100

    adding mediaquery layout

--- js/detail.js
+++ js/detail.js
@@ -1,19 +1,7 @@
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const countryName = params.get("name");
+//const countryName = "denmark";
 
 const url = `https://restcountries.com/v3.1/name/${countryName}`;
 console.log(url);
-
-async function countryApi() {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-
-    console.log(data);
-  } catch (error) {
-    console.log("This is:", error);
-    document.querySelector("body").innerHTML = `<h1>${error}</h1>`;
-  }
-}
-countryApi();
--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
-const url = "https://restcountries.com/v3.1/";
-// const urlName = "https://restcountries.com/v3.1/name";
-let query = "all";
+const urlName = "";
+const all = "all";
+let findCountry = "";
+
+const searchCountry = document.querySelector(".search-country");
+const buttonName = document.querySelector(".button-name");
+
 async function countryApi() {
   try {
-    const response = await fetch(url + query);
+    const response = await fetch(
+      `https://restcountries.com/v3.1/${all}/${urlName}/${findCountry}`
+    );
     const data = await response.json();
-
     console.log(data);
 
     getInfo(data);
+    byContinents(data);
   } catch (error) {
     console.log("This is:", error);
     document.querySelector("body").innerHTML = `<h1>${error}</h1>`;
@@ -16,11 +22,18 @@ async function countryApi() {
 }
 countryApi();
 
+buttonName.addEventListener("click", () => {
+  findCountry = searchCountry.value;
+  console.log("Click");
+  countryApi();
+});
+
 function getInfo(data) {
   data.forEach((country) => {
     //console.log(country.name.common);
     //console.log(country.flags.png);
     //console.log(country.capital);
+
     const container = document.createElement("div");
     container.classList.add("card-prop");
     const countryLink = document.createElement("a");
@@ -29,14 +42,18 @@ function getInfo(data) {
     const countryCapital = document.createElement("h3");
     //
     heading.textContent = country.name.common;
-    countryLink.href = `details.html?=${country.name}`;
+    countryLink.href = `details.html?=${findCountry}`;
     countryLink.textContent = `Link`;
     countryFlag.src = `${country.flags.png}`;
     countryCapital.textContent = `${country.capital}`;
     //
     container.append(heading, countryFlag, countryCapital, countryLink);
-
     document.querySelector(".container").append(container);
-    console.log(countryLink.href);
   });
+}
+
+function byContinents(data) {
+  for (let i = 0; i < data.length; i++) {
+    //console.log(data[i].continents[0]);
+  }
 }
