ac59a73e5293ebcd2e5638cf4ae7420c1817ae1c
mariusrundereim <marius.rundereim@gmail.com>
2023-02-27 12:18:25 +0100

    styling improvements

--- js/components/message.js
+++
@@ -1,6 +0,0 @@
-// function createMessage(type, message)
-
-function createMessage(type, message) {
-  const html = `<div class="message ${type}">${message}</div>`;
-  return html;
-}
--- js/detail.js
+++ js/detail.js
@@ -41,18 +41,7 @@ function getDetails(data) {
     <div class="detail-item">
     <h3>Population: <span class="api-result-focus">${country.population}</span></h3>
     </div>
-    <div class="detail-item"></div>
-    <div class="detail-item"></div>
-    <div class="map-container">
-    <iframe
-              src="${country.maps.googleMaps}"
-              width="100%"
-              height="100%"
-              style="border: 0"
-              allowfullscreen=""
-              loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </div>
+   
     
     
     
